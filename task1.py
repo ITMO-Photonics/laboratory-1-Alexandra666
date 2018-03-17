a3cd654db05182727bf20e955486481d7b9df39b
user1 <georgievaalexandra95@gmail.com>
2018-03-17 16:13:49 +0300

    done

--- task1.py
+++ task1.py
@@ -1,1 +1,12 @@
+import math # use math.pi from this module
 
+# Function for calculation of volume of a cylinder 
+
+
+def calculate(R, L):
+    volume = math.pi * R * R * L
+    return ('Volume: ' +  str(volume))
+R = int(input('Enter radius: '))
+L = int(input('Enter length: '))
+
+print(calculate(R, L))
