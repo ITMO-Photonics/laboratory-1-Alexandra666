26fd7873a62a378d8ccd3efaccc6f7825e30d7e8
user1 <georgievaalexandra95@gmail.com>
2018-03-17 16:14:36 +0300

    done

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
+import numpy as np
+from scipy import linalg
+
+n=int(input('Enter number of equations '))
+A=np.zeros((n,n))
+i,j=np.indices((n,n))
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+2]=1
+B=np.arange(n)
+x=linalg.solve(A,B)
+print(x)
+#Check that the solution is correct:
+
 
